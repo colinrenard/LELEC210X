4877219901a4105cdf4f7d4815e9dc90a0fdc7b4
Mathieu Reniers <mathieu.reniers@student.uclouvain.be>
2023-12-19 09:44:42 +0100

    chaine complete

--- classification/src/classification/__main__.py
+++ classification/src/classification/__main__.py
@@ -1,7 +1,7 @@
 import pickle
 from pathlib import Path
 from typing import Optional
-
+import numpy as np
 import click
 
 import common
@@ -9,6 +9,60 @@ from common.logging import logger
 from auth import PRINT_PREFIX
 
 from .utils import payload_to_melvecs
+import matplotlib.pyplot as plt
+
+def plot_specgram(
+    specgram,
+    ax,
+    is_mel=False,
+    title=None,
+    xlabel="Time index",
+    ylabel="Frequency [Hz]",
+    cmap="jet",
+    cb=True,
+    tf=None,
+    invert=True,
+):
+    """Plot a spectrogram (2D matrix) in a chosen axis of a figure.
+    Inputs:
+        - specgram = spectrogram (2D array)
+        - ax       = current axis in figure
+        - title
+        - xlabel
+        - ylabel
+        - cmap
+        - cb       = show colorbar if True
+        - tf       = final time in xaxis of specgram
+    """
+
+    if tf is None:
+        tf = specgram.shape[1]
+
+    if is_mel:
+        ylabel = "Frequency [Mel]"
+        im = ax.imshow(
+            specgram, cmap=cmap, aspect="auto", extent=[0, tf, specgram.shape[0], 0]
+        )
+    else:
+        im = ax.imshow(
+            specgram,
+            cmap=cmap,
+            aspect="auto",
+            extent=[0, tf, int(specgram.size / tf), 0],
+        )
+    if invert:
+        ax.invert_yaxis()
+
+    fig = plt.gcf()
+    if cb:
+        fig.colorbar(im, ax=ax)
+
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    ax.set_title(title)
+    return None
+
+
 
 
 @click.command()
@@ -51,18 +105,36 @@ def main(
     if model:
         with open(model, "rb") as file:
             m = pickle.load(file)
+            #m = pickle.load("classification/data/models/randomforest_1000.pickle")
     else:
         m = None
 
+    #m = pickle.load(open("classification/data/models/randomforest_1000.pickle","rb"))
     for payload in _input:
         if PRINT_PREFIX in payload:
             payload = payload[len(PRINT_PREFIX) :]
 
             melvecs = payload_to_melvecs(payload, melvec_length, n_melvecs)
-            logger.info(f"Parsed payload into Mel vectors: {melvecs}")
+            #logger.info(f"Parsed payload into Mel vectors: {melvecs}")
 
-            print(melvecs)
+            print(m)
 
             if m:
                 # TODO: perform classification
+                fv = melvecs
+                fv = fv.reshape(1,400)
+                print(fv.shape)
+                prediction = m.predict(fv)
+                probas = m.predict_proba(fv)
+                print(probas)
+                print(prediction[0])
+                f, ax = plt.subplots(1, 1, figsize=(10, 5))
+                plot_specgram(
+                    fv.reshape((20, 20)),
+                    ax,
+                    is_mel=True,
+                    title="MEL Spectrogram"
+                )
+                plt.pause(1)
+                plt.clf()
                 pass
